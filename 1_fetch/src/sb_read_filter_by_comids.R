c332e6f54452df5b7a7c7307060b844538929afb
Margaux Sleckman <msleckman@usgs.gov>
2022-06-08 15:26:57 -0700

    adding error catch

--- 1_fetch/src/sb_read_filter_by_comids.R
+++ 1_fetch/src/sb_read_filter_by_comids.R
@@ -7,19 +7,22 @@ sb_read_filter_by_comids <- function(data_path = '1_fetch/out/statsgo', sb_comid
   #' @param data_path folder path to files downloaded from sb
   #' @param cbind if true read in files are merged by column COMID
   
-  ##unzip - if no zip this will 
+  # 1) unzip - if no zip this will pass 
   data_zip_lst <- list.files(path = data_path, pattern = '*.zip', full.names = TRUE)
   
   if(!is_empty(data_zip_lst)){
-  ##unzipping in sale location as zipped. No subfolder created 
+  ##unzipping in same location as zipped. No subfolder created 
   lapply(data_zip_lst, function(x) unzip(x, exdir = data_path))
   }
-
+  
+  # 2) read
   ## Pull downloaded files
   data_lst <- list.files(path = data_path, pattern = '*.TXT|*.csv', full.names = TRUE)
-  
+  if(is_empty(data_lst)){
+    stop('No txt or csv file in this data path')
+  }
   ## read in + clip
-  data <- lapply(data_lst[1:4], function(x) read.delim(x, sep = ',') %>% 
+  data <- lapply(data_lst[1:4], function(x) read.csv(x, sep = ',') %>% 
                    filter(.data[[sb_comid_col]] %in% comid))
                  
   ## cbind if true
