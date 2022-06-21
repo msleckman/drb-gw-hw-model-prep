d393736e8a2bad626d3c2d91d71f0da5a8481b87
Koenig <lkoenigsnyder@usgs.gov>
2022-06-21 15:43:59 -0400

    read in crosswalk table that includes segidnat

--- 1_fetch.R
+++ 1_fetch.R
@@ -4,25 +4,15 @@ p1_targets_list <- list(
   
   # Read in the NHM - NHDv2 crosswalk file
   tar_target(
-    p1_GFv1_NHDv2_xwalk_csv,
-    # For now, download the NHM/PRMS - NHDv2 crosswalk table with divergent
-    # reaches omitted from the GW/HW Quest "Data" folder on Sharepoint and
-    # place in a directory called 1_fetch/in. The crosswalk table will 
-    # ultimately be pulled directly from the drb-network-prep github repo.
-    "1_fetch/in/GFv1_NHDv2_xwalk.csv",
-    format = "file"
-  ),
-  
-  tar_target(
     p1_GFv1_NHDv2_xwalk,
-    read_csv(p1_GFv1_NHDv2_xwalk_csv,col_types = cols(.default = "c"))
+    read_csv(GFv1_NHDv2_xwalk_url,col_types = cols(.default = "c"))
   ),
   
   # Reshape crosswalk table to return all COMIDs that overlap each NHM segment
   tar_target(
     p1_drb_comids_segs, 
     p1_GFv1_NHDv2_xwalk %>%
-      select(PRMS_segid, comid_seg) %>% 
+      select(PRMS_segid, segidnat, comid_seg) %>% 
       tidyr::separate_rows(comid_seg,sep=";") %>% 
       rename(COMID = comid_seg)
   ),
@@ -31,7 +21,7 @@ p1_targets_list <- list(
   tar_target(
     p1_drb_comids_all_tribs, 
     p1_GFv1_NHDv2_xwalk %>%
-      select(PRMS_segid, comid_cat) %>% 
+      select(PRMS_segid, segidnat, comid_cat) %>% 
       tidyr::separate_rows(comid_cat,sep=";") %>% 
       rename(COMID = comid_cat)
   ),
