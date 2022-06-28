1e24c917a6b157e56c0c04e65973f1ff011b9e80
Koenig <lkoenigsnyder@usgs.gov>
2022-06-28 13:36:48 -0400

    delete site-to-reach function that was superseded by subset_closest_nhd

--- 2_process.R
+++ 2_process.R
@@ -1,20 +1,7 @@
-source("2_process/src/match_sites_to_reaches.R")
 source("2_process/src/subset_closest_nhd.R")
 
 p2_targets_list <- list(
-  
-  # Match temperature monitoring locations to NHDPlusv2 flowline reaches.
-  # Use the previously-matched NHM segment when matching sites to 
-  # NHDv2 reaches; note that this will result in sites matched to NHDv2
-  # reaches that are up to 10 km away from the site. 
-  tar_target(
-    p2_drb_temp_sites_w_segs_use_NHM_match,
-    match_sites_to_reaches(nhd_lines = p1_nhd_reaches, 
-                           sites = p1_drb_temp_sites_sf, 
-                           comids_segs = p1_drb_comids_segs,
-                           use_NHM_match = TRUE)
-  ),
-  
+
   # Match temperature monitoring locations to "mainstem" NHDPlusv2 flowline
   # reaches, i.e. those that NHD reaches that intersect the NHM river network.
   # Note that this site-to-segment matching procedure emulates the process
