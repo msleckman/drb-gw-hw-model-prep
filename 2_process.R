1e85852957e1670afe985dd5c2d6e03af2c18658
Koenig <lkoenigsnyder@usgs.gov>
2022-07-29 19:46:05 -0400

    move formatting steps in p2_input_drivers_nhd to separate function

--- 2_process.R
+++ 2_process.R
@@ -2,6 +2,7 @@ source("2_process/src/subset_closest_nhd.R")
 source('2_process/src/raster_in_polygon_weighted_mean.R')
 source("2_process/src/estimate_mean_width.R")
 source("2_process/src/write_data.R")
+source("2_process/src/combine_nhd_input_drivers.R")
 
 p2_targets_list <- list(
 
@@ -103,34 +104,14 @@ p2_targets_list <- list(
   ),
   
   # Compile river-dl input drivers at NHDv2 resolution, including river width
-  # (meters), slope (unitless), and min/max elevation (transformed to meters
-  # from cm). Note that these input drivers represent "mainstem" NHDv2 reaches 
-  # only (i.e., those reaches that intersect the NHM fabric).
+  # (meters), slope (unitless), and min/max elevation (meters). Note that these
+  # input drivers represent "mainstem" NHDv2 reaches only (i.e., those reaches
+  # that intersect the NHM fabric).
   tar_target(
     p2_input_drivers_nhd,
-    p2_nhd_mainstem_reaches_w_width %>%
-      sf::st_drop_geometry() %>%
-      mutate(COMID = as.character(comid),
-             min_elev_m = minelevsmo/100, 
-             max_elev_m = maxelevsmo/100,
-             slope = if_else(slope == -9998, NA_real_, slope)) %>%
-      # add NHM segment identifier segidnat
-      left_join(y = p1_drb_comids_all_tribs, by = "COMID") %>%
-      rename(subsegid = PRMS_segid) %>%
-      # calculate length-weighted average slope for NHDv2 reaches associated
-      # with each NHM reach. For simplicity, weight by the reach length rather
-      # than another value-added attribute, slopelenkm, which represents the
-      # length over which the NHDv2 attribute slope was computed.
-      group_by(subsegid) %>%
-      mutate(slope_len_wtd_mean = weighted.mean(x = slope, w = lengthkm, na.rm = TRUE),
-             seg_width_max = max(est_width_m, na.rm = TRUE), 
-             seg_elev_min = min(min_elev_m, na.rm = TRUE)) %>%
-      ungroup() %>%
-      # join select attributes from PRMS-SNTemp
-      left_join(y = p2_input_drivers_prms, by = "segidnat") %>%
-      select(COMID, segidnat, subsegid, est_width_m, slope, slopelenkm, slope_len_wtd_mean, 
-             lengthkm, min_elev_m, max_elev_m, seg_elev, seg_slope, seg_width, seg_width_max,
-             seg_elev_min) 
+    combine_nhd_input_drivers(p2_nhd_mainstem_reaches_w_width,
+                              p2_input_drivers_prms,
+                              p1_drb_comids_all_tribs)
   ),
   
   # Save river-dl input drivers at NHDv2 resolution as a feather file
---
+++ 2_process/src/combine_nhd_input_drivers.R
@@ -0,0 +1,64 @@
+#' @title Combine NHD input drivers
+#' 
+#' @description 
+#' Function to combine river-dl input drivers for NHDPlusv2 reaches, including
+#' mean width, reach slope, reach elevation, and meteorological data.
+#' 
+#' @param nhd_flowlines sf object containing NHDPlusv2 flowline reaches. Must
+#' contain columns "comid", "minelevsmo", "maxelevsmo", "slope"
+#' @param prms_inputs data frame containing input drivers for each NHM segment.
+#' Must include columns "segidnat", "seg_elev", "seg_slope", and "seg_width"
+#' @param nhd_nhm_xwalk data frame that specifies how NHDPlusv2 COMIDs map
+#' onto NHM segment identifiers. Must contain columns "COMID", "PRMS_segid",
+#' and "segidnat".
+#'
+#' @returns 
+#' Returns a data frame with one row per COMID in `nhd_flowlines`. Columns
+#' indicate the NHM segment identifiers, the mean width for the NHD reach 
+#' ("est_width_m"), the slope of the NHD reach ("slope"), the length-weighted
+#' NHD slope for the COMIDs that make up each NHM segment ("slope_len_wtd_mean"), 
+#' the length of the NHD reach ("lengthkm"), the min and max NHD reach elevation
+#' ("min_elev_m" and "max_elev_m"), the elevation, slope, and mean width of the 
+#' corresponding NHM segment ("seg_elev", "seg_slope", and "seg_width"), the
+#' maximum NHD reach width among the COMIDs that make up each NHM segment
+#' ("seg_width_max"), and the minimum NHD slope among the COMIDs that make up
+#' each NHM segment ("seg_elev_min").
+#' 
+combine_nhd_input_drivers <- function(nhd_flowlines, prms_inputs, nhd_nhm_xwalk){
+  
+  # Subset NHDPlusv2 flowlines to return the desired attributes, 
+  # including elevation and slope
+  nhd_attributes <- nhd_flowlines %>%
+    sf::st_drop_geometry() %>%
+    # transform elevation from cm to meters
+    mutate(COMID = as.character(comid),
+           min_elev_m = minelevsmo/100, 
+           max_elev_m = maxelevsmo/100,
+           slope = if_else(slope == -9998, NA_real_, slope)) 
+  
+  # Add NHM segment identifier to NHDv2 attributes table
+  nhd_attributes_w_nhm <- nhd_attributes %>%
+    left_join(y = nhd_nhm_xwalk, by = "COMID") %>%
+    rename(subsegid = PRMS_segid)
+  
+  # Format NHDPlusv2 input data.
+  # calculate length-weighted average slope for NHDv2 reaches associated
+  # with each NHM reach. For simplicity, weight by the reach length rather
+  # than another value-added attribute, slopelenkm, which represents the
+  # length over which the NHDv2 attribute slope was computed.
+  nhd_static_inputs <- nhd_attributes_w_nhm %>%
+    group_by(subsegid) %>%
+    mutate(slope_len_wtd_mean = weighted.mean(x = slope, w = lengthkm, na.rm = TRUE),
+           seg_width_max = max(est_width_m, na.rm = TRUE), 
+           seg_elev_min = min(min_elev_m, na.rm = TRUE)) %>%
+    ungroup() %>%
+    # join select attributes from PRMS-SNTemp
+    left_join(y = prms_inputs, by = "segidnat") %>%
+    select(COMID, segidnat, subsegid, est_width_m, slope, slope_len_wtd_mean, 
+           lengthkm, min_elev_m, max_elev_m, seg_elev, seg_slope, seg_width, seg_width_max,
+           seg_elev_min)
+  
+  return(nhd_static_inputs)
+
+}
+
